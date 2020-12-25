0c7cf813148b5c66d21930d079d4b8ece8d45566
Kaustubh <kaustubh@Kaustubh-Vinzanekar-Mac-Pro.local>
2020-12-25 17:44:43 -0500

    Updated ListingPage with fake data

--- buy-and-sell/src/app/listings-page/listings-page.component.ts
+++ buy-and-sell/src/app/listings-page/listings-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Listing} from '../types';
+import { fakeListings } from '../fake-data';
+
 @Component({
   selector: 'app-listings-page',
   templateUrl: './listings-page.component.html',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListingsPageComponent implements OnInit {
 
+  listings: Listing[] = [];
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.listings = fakeListings;
   }
 
 }
